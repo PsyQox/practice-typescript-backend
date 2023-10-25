edc9277fc12d046fe06ae4c9fb6fbad929865a25
PsyQox <pikithor@gmail.com>
2023-10-25 14:23:06 -0500

    terminado

--- build/index.js
+++ build/index.js
@@ -4,13 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const diaries_1 = __importDefault(require("./routes/diaries"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const PORT = 3001;
-app.get('/pign', (_req, res) => {
+app.get('/ping', (_req, res) => {
     console.log("Prueba");
     res.send("<h1>Hola</h1>");
 });
+app.use('/api/diaries', diaries_1.default);
 app.listen(PORT, () => {
     console.log(`Server ON! ${PORT}`);
 });
